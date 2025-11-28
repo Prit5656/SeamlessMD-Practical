ad74a38fb3047da4eebcad82de95ab411e0ac74e
Prit5656 <prit.patel1104@gmail.com>
2025-11-27 22:47:47 -0500

    Update README and support files

--- CypressAutomation/cypress/support/e2e.js
+++ CypressAutomation/cypress/support/e2e.js
@@ -4,7 +4,7 @@ import './login-commands'
 
 Cypress.on('fail', (error) => {
   console.error('Test failed but continuing:', error.message);
-  return false;  // prevents Cypress from stopping
+  return false;
 });
 
 
